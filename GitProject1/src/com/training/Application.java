811b23e0b7b201fe26fe0daf3d1e9428b32a6e19
Shivani pai <shivanipai134@gmail.com>
2019-11-06 12:49:39 +0530

    First Commit

---
+++ GitProject1/src/com/training/Application.java
@@ -0,0 +1,15 @@
+package com.training;
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		List<String> nameList=Arrays.asList("Shivani","Nidhi","Navneet");
+		nameList.forEach(System.out::println);
+		
+
+	}
+
+}
